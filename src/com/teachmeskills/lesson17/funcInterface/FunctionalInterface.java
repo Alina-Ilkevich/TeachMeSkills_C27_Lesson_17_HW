38dfc2b67b72588ee35e72e0948e0e46d988e9f8
Alina-Ilkevich <alina.1qaz@gmail.com>
2024-03-02 19:17:56 +0300

    init commit

---
+++ src/com/teachmeskills/lesson17/funcInterface/FunctionalInterface.java
@@ -0,0 +1,9 @@
+package com.teachmeskills.lesson17.funcInterface;
+
+/**
+ * This is a functional interface with one method
+ */
+@java.lang.FunctionalInterface
+public interface FunctionalInterface <T> {
+    T doSomething (T obj);
+}
---
+++ src/com/teachmeskills/lesson17/run/Run.java
@@ -0,0 +1,45 @@
+package com.teachmeskills.lesson17.run;
+
+import com.teachmeskills.lesson17.funcInterface.FunctionalInterface;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * This class contains a method for reversing a string
+ * or finding the factorial of a number depending on the number entered
+ */
+public class Run {
+    public static void main(String[] args) {
+        System.out.print("Enter the number 1 or 2: ");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int num = scanner.nextInt();
+            if (num == 1){
+                FunctionalInterface<String> a;
+                a = (str) -> {
+                    return new StringBuilder(str).reverse().toString();
+                };
+                System.out.println(a.doSomething("Alina"));
+            } else if (num == 2){
+                FunctionalInterface<Integer> b;
+                b = (number) -> {
+                    int count = 1;
+                    if (number == 0){
+                        count = 1;
+                    }
+                    for(int i = 1; i <= number; i++){
+                        count *= i;
+                    }
+                    return count;
+                };
+                System.out.println(b.doSomething(4));
+            } else {
+                System.out.println("You must enter the number 1 or 2");
+            }
+        }
+        catch (InputMismatchException e){
+            System.out.println("You must enter the number 1 or 2");
+        }
+    }
+}
